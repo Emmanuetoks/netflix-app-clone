d16178206b504302c97fda1d080782824eb9e8c7
Kevin Quaedvlieg <k.quaedvlieg@outlook.com>
2023-06-24 21:01:23 +0200

    Added netflix colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,15 @@ module.exports = {
     extend: {
       padding: {
         '15': '60px'
+      },
+      colors: {
+        n_primary: "#e50914",
+        n_dark_gray: "#6D6D6E",
+        n_light_gray: {
+          base: "#E5E5E5",
+          hover: "#b3b3b3"
+        },
+        n_black: "#141414"
       }
     }
   },
