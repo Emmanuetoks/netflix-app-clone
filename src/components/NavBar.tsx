e3de59ac06df8985417ee648dbbccfd8e3413972
Kevin Quaedvlieg <k.quaedvlieg@outlook.com>
2023-06-24 17:40:29 +0200

    Implemented correct navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,58 @@
+'use client'
+
 import Logo from "@/components/Logo";
-import Button from "@/components/Button";
+import NavItem from "@/components/NavItem";
+import {usePathname} from "next/navigation";
 
 export default function NavBar() {
+    const pathName = usePathname();
+
+    const navItems = [
+        {
+            href: "/",
+            label: "Home"
+        },
+        {
+            href: "/tv-shows",
+            label: "TV Shows"
+        },
+        {
+            href: "/movies",
+            label: "Movies"
+        },
+        {
+            href: "/new-and-popular",
+            label: "New & Popular"
+        },
+        {
+            href: "/my-list",
+            label: "My List"
+        },
+        {
+            href: "/browse-by-languages",
+            label: "Browse by languages"
+        }
+    ]
+
     return (
-        <nav className="container mx-auto max-w-[1172px] flex justify-between items-center h-[86px]">
-            <Logo className="fill-red-600 h-[40px]"/>
-            <Button>Sign in</Button>
+        <nav className="w-full flex justify-between items-center h-[68px] px-15 bg-black">
+            {/* Logo and nav items */}
+            <div className="flex">
+                <Logo className="fill-red-600 h-[31px] w-[92.5px] mr-[25px]"/>
+                {/* nav ite,s */}
+                <div className="flex align-center p-0">
+                    {
+                        navItems.map((navItem) => (
+                            <NavItem href={navItem.href} key={navItem.href} active={pathName === navItem.href}>{navItem.label}</NavItem>
+                        ))
+                    }
+                </div>
+            </div>
+
+            {/* Search, notifications & profile */}
+            <div>
+
+            </div>
         </nav>
     )
 }
