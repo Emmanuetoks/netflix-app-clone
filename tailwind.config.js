27917e8918befec6b91a69ba7c36020ddb4b1785
Gabrielius Batakis <gabrieliusbatakis@gmail.com>
2023-06-25 21:36:28 +0300

    added variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      screens: {
+        "2sm": "500px",
+        "2md": "900px",
+      },
       padding: {
-        '15': '60px'
-      }
-    }
+        15: "60px",
+      },
+      colors: {
+        n_primary: "#e50914",
+        n_dark_gray: "#6D6D6E",
+        n_light_gray: {
+          base: "#E5E5E5",
+          hover: "#b3b3b3",
+        },
+        n_black: "#141414",
+      },
+    },
   },
   plugins: [],
-}
+};
