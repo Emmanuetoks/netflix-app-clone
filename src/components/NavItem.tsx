2b18bd695d2602467d4d5955e0d1ed13af86f596
Gabrielius Batakis <gabrieliusbatakis@gmail.com>
2023-06-25 21:39:20 +0300

    adjusted component in order to re-use it in footer

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -2,12 +2,29 @@ import React from "react";
 import Link from "next/link";
 
 interface NavItemProps {
-    children: React.ReactNode,
-    active?: boolean,
-    href: string
+  children: React.ReactNode;
+  active?: boolean;
+  href: string;
+  role: "header" | "footer";
 }
-export default function NavItem({children, active, href}: NavItemProps) {
-    return (
-        <Link href={href} className={`text-base ml-5 ${active ? 'text-white' : 'text-gray-300'} hover:text-gray-400`}>{children}</Link>
-    )
+export default function NavItem({
+  children,
+  active,
+  href,
+  role,
+}: NavItemProps) {
+  return (
+    <Link
+      href={href}
+      className={`${
+        role === "header"
+          ? `text-base ml-5 hover:text-gray-400 ${
+              active ? "text-white" : "text-gray-300"
+            }`
+          : "text-sm text-[gray] hover:underline"
+      } `}
+    >
+      {children}
+    </Link>
+  );
 }
